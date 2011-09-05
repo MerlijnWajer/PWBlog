aaf241c674805a4f2f01a88383f5ee6017accc9c
Merlijn Wajer <merlijn@wizzup.org>
2011-09-06 00:45:34 +0200

    Allow '-' in blog file names.

--- rules.py
+++ rules.py
@@ -3,10 +3,10 @@ import re
 wt.add_rule(re.compile('^%s/(.*)$' % STATIC_URL), static_serve,
                 ['static_file'])
 
-wt.add_rule(re.compile('^%s/blog/([a-za-z0-9]+)$' % BASE_URL), blog_page,
+wt.add_rule(re.compile('^%s/blog/([a-za-z0-9\-]+)$' % BASE_URL), blog_page,
                 ['entry'])
 
-wt.add_rule(re.compile('^%s/page/([a-za-z0-9]+)$' % BASE_URL), page_page,
+wt.add_rule(re.compile('^%s/page/([a-za-z0-9\-]+)$' % BASE_URL), page_page,
                 ['entry'])
 
 # This should be the last rule.
